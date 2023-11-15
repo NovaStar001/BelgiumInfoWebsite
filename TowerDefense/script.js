78d47e18b12313d8e525d790e50a5a3508fbd0eb
NovaStar001 <146793624+NovaStar001@users.noreply.github.com>
2023-11-15 13:25:20 -0600

    Update script.js

--- TowerDefense/script.js
+++ TowerDefense/script.js
@@ -1,19 +1,14 @@
 let money = 100;
 let selectedCell;
 
+// ... (Previous code remains unchanged)
+
 function buildTower() {
   if (selectedCell && selectedCell.classList.contains('empty-cell') && money >= 50) {
-    // Deduct $50 from the player's money
     money -= 50;
-
-    // Update the displayed money value
     document.getElementById('money').textContent = money;
-
-    // Place a tower in the selected cell
     selectedCell.classList.remove('empty-cell');
     selectedCell.classList.add('tower-cell');
-
-    // Reset selected cell
     selectedCell = null;
   } else if (!selectedCell) {
     alert('Select a cell to place the tower.');
@@ -24,16 +19,71 @@ function buildTower() {
   }
 }
 
+function spawnEnemy() {
+  const gridCells = document.querySelectorAll('.cell');
+  const emptyCells = Array.from(gridCells).filter(cell => cell.classList.contains('empty-cell'));
+
+  if (emptyCells.length > 0) {
+    const randomCell = emptyCells[Math.floor(Math.random() * emptyCells.length)];
+    randomCell.classList.remove('empty-cell');
+    randomCell.classList.add('enemy-cell');
+    
+    // Move the enemy
+    moveEnemy(randomCell);
+  }
+}
+
+// ... (Previous code remains unchanged)
+
+function moveEnemy(enemyCell) {
+  const gridCells = document.querySelectorAll('.cell');
+  const currentIndex = Array.from(gridCells).indexOf(enemyCell);
+
+  // Move the enemy to the next cell
+  const nextIndex = currentIndex + 1;
+  const nextCell = gridCells[nextIndex];
+
+  if (nextCell && nextCell.classList.contains('empty-cell')) {
+    enemyCell.classList.remove('enemy-cell');
+    nextCell.classList.add('enemy-cell');
+
+    // Move the enemy to the next cell after a delay (you can adjust the delay based on your game speed)
+    setTimeout(() => moveEnemy(nextCell), 500);
+  } else {
+    // Enemy reached the end or another condition for game over
+    alert('Game Over!');
+    resetGame();
+  }
+}
+
+function resetGame() {
+  // Reset the grid and other game variables
+  const gridCells = document.querySelectorAll('.cell');
+  gridCells.forEach(cell => {
+    cell.classList.remove('tower-cell', 'enemy-cell');
+    cell.classList.add('empty-cell');
+  });
+
+  money = 100;
+  document.getElementById('money').textContent = money;
+
+  // Add more reset logic based on your game design
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   const gridContainer = document.getElementById('grid-container');
+  const startButton = document.getElementById('start-button');
+  const buildButton = document.getElementById('build-button');
 
   for (let i = 0; i < 50; i++) {
     const cell = document.createElement('div');
     cell.classList.add('cell', 'empty-cell');
-    cell.addEventListener('click', () => {
-      // Set the selected cell when clicked
-      selectedCell = cell;
-    });
     gridContainer.appendChild(cell);
   }
+
+  startButton.addEventListener('click', () => {
+    setInterval(spawnEnemy, 2000); // Spawn an enemy every 2 seconds (adjust based on your game speed)
+  });
+
+  buildButton.addEventListener('click', buildTower);
 });
