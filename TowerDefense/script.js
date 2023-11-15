1b996e95f405282653520abf63400b36e83c92b4
NovaStar001 <146793624+NovaStar001@users.noreply.github.com>
2023-11-15 13:17:14 -0600

    Update script.js

--- TowerDefense/script.js
+++ TowerDefense/script.js
@@ -1,13 +1,26 @@
 let money = 100;
+let selectedCell;
 
 function buildTower() {
-  if (money >= 50) {
+  if (selectedCell && selectedCell.classList.contains('empty-cell') && money >= 50) {
+    // Deduct $50 from the player's money
     money -= 50;
+
+    // Update the displayed money value
     document.getElementById('money').textContent = money;
 
-    alert('Tower built!');
+    // Place a tower in the selected cell
+    selectedCell.classList.remove('empty-cell');
+    selectedCell.classList.add('tower-cell');
+
+    // Reset selected cell
+    selectedCell = null;
+  } else if (!selectedCell) {
+    alert('Select a cell to place the tower.');
+  } else if (!selectedCell.classList.contains('empty-cell')) {
+    alert('This cell is already occupied.');
   } else {
-    alert('Not enough money to build a tower!');
+    alert('Not enough money to build a tower.');
   }
 }
 
@@ -16,9 +29,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
   for (let i = 0; i < 50; i++) {
     const cell = document.createElement('div');
-    cell.classList.add('cell');
+    cell.classList.add('cell', 'empty-cell');
     cell.addEventListener('click', () => {
-      alert('Clicked on cell ' + i);
+      // Set the selected cell when clicked
+      selectedCell = cell;
     });
     gridContainer.appendChild(cell);
   }
