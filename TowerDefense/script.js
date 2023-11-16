56d4aa99a0f4f4f138d9f6d0bbf12d4e9bbcb6ce
NovaStar001 <146793624+NovaStar001@users.noreply.github.com>
2023-11-16 09:05:33 -0600

    Update script.js

--- TowerDefense/script.js
+++ TowerDefense/script.js
@@ -1,10 +1,35 @@
+class Tower {
+  constructor(cell, attackPower, attackSpeed, range) {
+    this.cell = cell;
+    this.attackPower = attackPower;
+    this.attackSpeed = attackSpeed;
+    this.range = range;
+  }
+
+  attack(enemy) {
+    enemy.health -= this.attackPower;
+    if (enemy.health <= 0) {
+      enemy.cell.classList.remove('enemy-cell');
+      enemy.cell.classList.add('empty-cell');
+      // Handle enemy destruction, e.g., award money
+    }
+  }
+}
+
 let money = 100;
 let selectedCell;
+let towers = [];
+let enemies = [];
 
 function buildTower() {
   if (selectedCell && selectedCell.classList.contains('empty-cell') && money >= 50) {
     money -= 50;
     document.getElementById('money').textContent = money;
+
+    // Create a new tower and add it to the towers array
+    const tower = new Tower(selectedCell, 10, 1000, 3); // Example values, adjust as needed
+    towers.push(tower);
+
     selectedCell.classList.remove('empty-cell');
     selectedCell.classList.add('tower-cell');
     selectedCell = null;
@@ -25,35 +50,49 @@ function spawnEnemy() {
     const randomCell = emptyCells[Math.floor(Math.random() * emptyCells.length)];
     randomCell.classList.remove('empty-cell');
     randomCell.classList.add('enemy-cell');
-    
-    // Move the enemy
-    moveEnemy(randomCell);
+
+    // Create an enemy object and add it to the enemies array
+    const enemy = { cell: randomCell, health: 20 }; // Example values, adjust as needed
+    enemies.push(enemy);
   }
+}
+
+function attackEnemies() {
+  towers.forEach(tower => {
+    enemies.forEach(enemy => {
+      // Check if the enemy is within range
+      if (getDistance(tower.cell, enemy.cell) <= tower.range) {
+        tower.attack(enemy);
+      }
+    });
+  });
+}
+
+function getDistance(cell1, cell2) {
+  const index1 = Array.from(document.querySelectorAll('.cell')).indexOf(cell1);
+  const index2 = Array.from(document.querySelectorAll('.cell')).indexOf(cell2);
+  return Math.abs(index1 - index2);
 }
 
 function moveEnemy(enemyCell) {
   const gridCells = document.querySelectorAll('.cell');
   const currentIndex = Array.from(gridCells).indexOf(enemyCell);
-
-  // Move the enemy to the next cell
   const nextIndex = currentIndex + 1;
   const nextCell = gridCells[nextIndex];
 
   if (nextCell && nextCell.classList.contains('empty-cell')) {
     enemyCell.classList.remove('enemy-cell');
     nextCell.classList.add('enemy-cell');
-
-    // Move the enemy to the next cell after a delay (you can adjust the delay based on your game speed)
     setTimeout(() => moveEnemy(nextCell), 500);
   } else {
-    // Enemy reached the end or another condition for game over
     alert('Game Over!');
     resetGame();
   }
+
+  attackEnemies();
 }
 
 function resetGame() {
-  // Reset the grid and other game variables
   const gridCells = document.querySelectorAll('.cell');
   gridCells.forEach(cell => {
     cell.classList.remove('tower-cell', 'enemy-cell');
@@ -63,7 +102,8 @@ function resetGame() {
   money = 100;
   document.getElementById('money').textContent = money;
 
-  // Add more reset logic based on your game design
+  towers = [];
+  enemies = [];
 }
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -75,14 +115,13 @@ document.addEventListener('DOMContentLoaded', () => {
     const cell = document.createElement('div');
     cell.classList.add('cell', 'empty-cell');
     cell.addEventListener('click', () => {
-      // Set the selected cell when clicked
       selectedCell = cell;
     });
     gridContainer.appendChild(cell);
   }
 
   startButton.addEventListener('click', () => {
-    setInterval(spawnEnemy, 2000); // Spawn an enemy every 2 seconds (adjust based on your game speed)
+    setInterval(spawnEnemy, 2000);
   });
 
   buildButton.addEventListener('click', buildTower);
